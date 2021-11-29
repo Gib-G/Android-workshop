ce3b6a8511cb1fcf0763f0435d7ac2bff9653004
Gibril Gunder <gibril.gunder@telecom-sudparis.eu>
2021-11-29 17:19:18 +0100

    Task creation form button sends an intent back to the main activity with a newly created Task instance in it

--- app/src/main/java/com/gib/filrouge/form/FormActivity.kt
+++ app/src/main/java/com/gib/filrouge/form/FormActivity.kt
@@ -2,11 +2,26 @@ package com.gib.filrouge.form
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import com.gib.filrouge.R
+import com.gib.filrouge.tasklist.Task
+import java.util.*
 
 class FormActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_form)
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+
+        val validateButton = findViewById<Button>(R.id.validateTaskCreation);
+
+        validateButton.setOnClickListener({
+            // Instanciation d'un nouvel objet [Task]
+            val newTask = Task(id = UUID.randomUUID().toString(), title = "New Task !")
+            intent.putExtra("task", newTask);
+            setResult(RESULT_OK, intent);
+            finish();
+        });
     }
 }
