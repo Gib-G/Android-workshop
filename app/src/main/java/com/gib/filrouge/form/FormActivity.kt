c804f0514efbbd33bb9867fcea8b977d522f30c2
Gibril Gunder <gibril.gunder@telecom-sudparis.eu>
2021-11-29 17:38:01 +0100

    Set the given title and description to the new created task in the form

--- app/src/main/java/com/gib/filrouge/form/FormActivity.kt
+++ app/src/main/java/com/gib/filrouge/form/FormActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import com.gib.filrouge.R
 import com.gib.filrouge.tasklist.Task
 import java.util.*
@@ -17,8 +18,13 @@ class FormActivity : AppCompatActivity() {
         val validateButton = findViewById<Button>(R.id.validateTaskCreation);
 
         validateButton.setOnClickListener({
+
+            val taskTitle = findViewById<EditText>(R.id.taskNameInput).text.toString();
+            val taskDescription = findViewById<EditText>(R.id.taskDescriptionInput).text.toString();
+
             // Instanciation d'un nouvel objet [Task]
-            val newTask = Task(id = UUID.randomUUID().toString(), title = "New Task !")
+            val newTask = Task(id = UUID.randomUUID().toString(), title = taskTitle, description = taskDescription);
+            
             intent.putExtra("task", newTask);
             setResult(RESULT_OK, intent);
             finish();
