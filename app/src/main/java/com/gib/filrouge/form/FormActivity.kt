0dbaa08a7c1d408e8fd56a304aaae6fbc464ce82
Gibril Gunder <gibril.gunder@telecom-sudparis.eu>
2021-11-29 16:15:11 +0100

    Imported missing dependencies in FormActivity.kt

--- app/src/main/java/com/gib/filrouge/form/FormActivity.kt
+++ app/src/main/java/com/gib/filrouge/form/FormActivity.kt
@@ -2,6 +2,7 @@ package com.gib.filrouge.form
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gib.filrouge.R
 
 class FormActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
