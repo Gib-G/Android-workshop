1a843c8041170ddbd4aa156c86ebd372408fcbe5
Gibril Gunder <gibril.gunder@telecom-sudparis.eu>
2021-12-06 17:27:09 +0100

    Made Task class KotlinX-serializable

--- app/src/main/java/com/gib/filrouge/network/UserInfo.kt
+++ app/src/main/java/com/gib/filrouge/network/UserInfo.kt
@@ -4,11 +4,14 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class UserInfo(@SerialName("email")
-                    val email: String,
-                    @SerialName("firstname")
-                    val firstName: String,
-                    @SerialName("lastname")
-                    val lastName: String)
+data class UserInfo
+    (
+    @SerialName("email")
+    val email: String,
+    @SerialName("firstname")
+    val firstName: String,
+    @SerialName("lastname")
+    val lastName: String
+    )
 {
 }
