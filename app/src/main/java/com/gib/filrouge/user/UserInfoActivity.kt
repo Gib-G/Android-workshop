022aa62827991426965676bb90cb2b3f029e81f1
Gibril Gunder <gibril.gunder@telecom-sudparis.eu>
2021-12-27 21:48:48 +0100

    Added new activity to display user info

---
+++ app/src/main/java/com/gib/filrouge/user/UserInfoActivity.kt
@@ -0,0 +1,12 @@
+package com.gib.filrouge.user
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gib.filrouge.R
+
+class UserInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_info)
+    }
+}
