e35bbd286e6cc96c8c4e52f2a8281f5aa64c5520
Gibril Gunder <gibril.gunder@telecom-sudparis.eu>
2021-12-27 22:14:28 +0100

    Managing camera permission to take a picture to edit the user's avatar

--- app/src/main/java/com/gib/filrouge/user/UserInfoActivity.kt
+++ app/src/main/java/com/gib/filrouge/user/UserInfoActivity.kt
@@ -1,12 +1,77 @@
 package com.gib.filrouge.user
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
 import com.gib.filrouge.R
 
 class UserInfoActivity : AppCompatActivity() {
+
+    private var takePictureButton: Button? = null;
+    private var uploadImageButton: Button? = null;
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_info)
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_info);
+
+        takePictureButton = findViewById(R.id.take_picture_button);
+        uploadImageButton = findViewById(R.id.upload_image_button);
+
+        takePictureButton?.setOnClickListener {
+            launchCameraWithPermission();
+        }
     }
+
+    private val cameraPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { accepted ->
+            if (accepted) launchCamera();
+            else showExplanation();
+        }
+
+    private fun launchCameraWithPermission() {
+        val camPermission = Manifest.permission.CAMERA
+        val permissionStatus = checkSelfPermission(camPermission)
+        val isAlreadyAccepted = permissionStatus == PackageManager.PERMISSION_GRANTED
+        val isExplanationNeeded = shouldShowRequestPermissionRationale(camPermission)
+        when {
+            isAlreadyAccepted -> launchCamera(); // lancer l'action souhaitée
+                isExplanationNeeded -> showExplanation(); // afficher une explication
+            else -> launchAppSettings(); // lancer la demande de permission
+        }
+    }
+
+    private fun showExplanation() {
+        // ici on construit une pop-up système (Dialog) pour expliquer la nécessité de la demande de permission
+        AlertDialog.Builder(this)
+            .setMessage("🥺 On a besoin de la caméra, vraiment! 👉👈")
+            .setPositiveButton("Bon, ok") { _, _ -> /* ouvrir les paramètres de l'app */ }
+            .setNegativeButton("Nope") { dialog, _ -> dialog.dismiss() }
+            .show()
+    }
+
+    private fun launchAppSettings() {
+        // Cet intent permet d'ouvrir les paramètres de l'app (pour modifier les permissions déjà refusées par ex)
+        val intent = Intent(
+            Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+            Uri.fromParts("package", packageName, null)
+        )
+        // ici pas besoin de vérifier avant car on vise un écran système:
+        startActivity(intent)
+    }
+
+    private fun handleImage(imageUri: Uri) {
+        // afficher l'image dans l'ImageView
+    }
+
+    private fun launchCamera() {
+        // à compléter à l'étape suivante
+    }
+
 }
