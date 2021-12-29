52cedacee6590100bcef7dd8001bcaf2657052d2
Gibril Gunder <gibril.gunder@telecom-sudparis.eu>
2021-12-29 22:27:28 +0100

    Configured the main activity to redirect to the auth activity in case the API token is not found in shared preferences

--- app/src/main/java/com/gib/filrouge/MainActivity.kt
+++ app/src/main/java/com/gib/filrouge/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.gib.filrouge
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.edit
+import com.gib.filrouge.network.Api
+import com.gib.filrouge.user.AuthenticationActivity
 
+// The activity launched whe the user launches the app.
 class MainActivity : AppCompatActivity() {
+
+    // The launcher used to launch the authentication activity
+    // if the API token is not found in shared preferences.
+    private val activityLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Retrieves the API token from shared preferences.
+        val token = PreferenceManager.getDefaultSharedPreferences(Api.appContext).getString("auth_token_key", "")
+        // If the token does not exist, we redirect to the authentication
+        // activity to retrieve it from the API via a login or signup process.
+        if(token == "") {
+            activityLauncher.launch(Intent(this, AuthenticationActivity::class.java))
+        }
     }
+    
 }
