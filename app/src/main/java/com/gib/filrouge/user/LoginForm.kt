5c15ff85aa0822205b80c9e18067d82f34116e21
Gibril Gunder <gibril.gunder@telecom-sudparis.eu>
2021-12-29 16:19:24 +0100

    Added serialization annotations to data classes LoginForm and LoginResponse

--- app/src/main/java/com/gib/filrouge/user/LoginForm.kt
+++ app/src/main/java/com/gib/filrouge/user/LoginForm.kt
@@ -1,6 +1,12 @@
 package com.gib.filrouge.user
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class LoginForm(
+    @SerialName("email")
     val email: String,
+    @SerialName("password")
     val password: String
 )
--- app/src/main/java/com/gib/filrouge/user/LoginResponse.kt
+++ app/src/main/java/com/gib/filrouge/user/LoginResponse.kt
@@ -1,5 +1,10 @@
 package com.gib.filrouge.user
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class LoginResponse(
+    @SerialName("token")
     val apiToken: String
 )
