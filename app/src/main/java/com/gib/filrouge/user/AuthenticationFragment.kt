70750fcf5e848fb0ec8456f40798e5473972b979
Gibril Gunder <gibril.gunder@telecom-sudparis.eu>
2021-12-29 14:57:31 +0100

    Some cleaning-up in AuthenticationFragment

--- app/src/main/java/com/gib/filrouge/user/AuthenticationFragment.kt
+++ app/src/main/java/com/gib/filrouge/user/AuthenticationFragment.kt
@@ -5,29 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import com.gib.filrouge.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AuthenticationFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AuthenticationFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private var loginButton: Button? = null
+    private var signupButton: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -38,23 +26,17 @@ class AuthenticationFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_authentication, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AuthenticationFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AuthenticationFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        loginButton = view.findViewById(R.id.fragment_authentication_login_button)
+        signupButton = view.findViewById(R.id.fragment_authentication_signup_button)
+        loginButton?.setOnClickListener {
+            findNavController().navigate(R.id.action_authenticationFragment_to_loginFragment)
+        }
+        signupButton?.setOnClickListener {
+            findNavController().navigate(R.id.action_authenticationFragment_to_signupFragment)
+        }
     }
+
 }
