ada7347d805145a3a9d0cd8ee098336c4bb3d89a
Gibril Gunder <gibril.gunder@telecom-sudparis.eu>
2021-12-31 12:22:47 +0100

    Implemented input verifs in FormActivity

--- app/src/main/java/com/gib/filrouge/form/FormActivity.kt
+++ app/src/main/java/com/gib/filrouge/form/FormActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import com.gib.filrouge.R
+import com.gib.filrouge.network.Api
 import com.gib.filrouge.tasklist.Task
 import java.util.*
 
@@ -38,18 +40,24 @@ class FormActivity : AppCompatActivity() {
         okButton = findViewById<Button>(R.id.activity_form_ok_button);
 
         okButton.setOnClickListener {
-            // We create a new Task instance with the
-            // data provided by the user in the form's fields,
-            // regardless of whether the user edits an existing task,
-            // or creates a new one.
-            val newTask = Task(task?.id ?: UUID.randomUUID().toString(), taskNameField.text.toString(), taskDescriptionField.text.toString())
+            // We check that the user has at least given a name to the task.
+            if(taskNameField.text.isEmpty()) {
+                Toast.makeText(Api.appContext, "You will need some name for your task, no?", Toast.LENGTH_LONG).show()
+            }
+            else {
+                // We create a new Task instance with the
+                // data provided by the user in the form's fields,
+                // regardless of whether the user edits an existing task,
+                // or creates a new one.
+                val newTask = Task(task?.id ?: UUID.randomUUID().toString(), taskNameField.text.toString(), taskDescriptionField.text.toString())
 
-            // The activity returns an intent to the main activity
-            // before it finishes. This intent contains the newly
-            // created task instance.
-            intent.putExtra("task", newTask)
-            setResult(RESULT_OK, intent);
-            finish();
+                // The activity returns an intent to the main activity
+                // before it finishes. This intent contains the newly
+                // created task instance.
+                intent.putExtra("task", newTask)
+                setResult(RESULT_OK, intent);
+                finish();
+            }
         }
     }
 
@@ -61,21 +69,22 @@ class FormActivity : AppCompatActivity() {
         // the main activity in the task list fragment.
         task = intent.getSerializableExtra("task") as Task?
 
+        // In any case, we set some hints for the form's fields.
+        taskNameField.hint = "Task name"
+        taskDescriptionField.hint = "Task description (optional)"
         // We then adapt the layout of the activity based on
         // whether the task received is null
         // (user wants to add a new task) or not (user wants
         // to edit an existing task).
         if(task == null) {
-            text.setText("Hooray! Let's add a new task!")
-            taskNameField.hint = "Task name"
-            taskDescriptionField.hint = "Task description"
-            okButton.setText("Create")
+            text.text = "Hooray! Let's add a new task!"
+            okButton.text = "Create"
         }
         else {
-            text.setText("Let's change a few things here...")
+            text.text = "Let's change a few things here..."
             taskNameField.setText(task?.title)
             taskDescriptionField.setText(task?.description)
-            okButton.setText("Save")
+            okButton.text = "Save"
         }
     }
 
