4ee2d62f8795878bf7cc8214b50f5f6009e376a1
Gibril Gunder <gibril.gunder@telecom-sudparis.eu>
2021-12-31 11:29:54 +0100

    Cleaned App.kt and the Api object

--- app/src/main/java/com/gib/filrouge/App.kt
+++ app/src/main/java/com/gib/filrouge/App.kt
@@ -5,6 +5,7 @@ import com.gib.filrouge.network.Api
 
 class App: Application() {
 
+    // Assigning the app's context to Api::appContext.
     override fun onCreate() {
         super.onCreate()
         Api.setUpContext(this)
--- app/src/main/java/com/gib/filrouge/network/Api.kt
+++ app/src/main/java/com/gib/filrouge/network/Api.kt
@@ -8,16 +8,27 @@ import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 
+// Object for communicating with the API.
 object Api {
 
-    // constantes qui serviront à faire les requêtes
+    // The context of the app.
+    lateinit var appContext: Context
+
+    // Called when the app is launched (cf. App::onCreate).
+    fun setUpContext(context: Context) {
+        appContext = context
+    }
+
     private const val BASE_URL = "https://android-tasks-api.herokuapp.com/api/"
 
-    // client HTTP
+    // HTTP client.
     private val okHttpClient by lazy {
         OkHttpClient.Builder()
             .addInterceptor { chain ->
-                // intercepteur qui ajoute le `header` d'authentification avec votre token:
+                // This adds the user's API token to any
+                // request to the API.
+                // The user's token is retrieved from shared preferences
+                // (key "auth_token_key").
                 val newRequest = chain.request().newBuilder()
                     .addHeader("Authorization", "Bearer ${PreferenceManager.getDefaultSharedPreferences(appContext).getString("auth_token_key", "")}")
                     .build()
@@ -26,37 +37,30 @@ object Api {
             .build()
     }
 
-    // sérializeur JSON: transforme le JSON en objets kotlin et inversement
+    // JSON serializer.
     private val jsonSerializer = Json {
         ignoreUnknownKeys = true
         coerceInputValues = true
     }
 
-    // instance de convertisseur qui parse le JSON renvoyé par le serveur:
+    // Converter parsing the JSON API responses.
     private val converterFactory =
         jsonSerializer.asConverterFactory("application/json".toMediaType())
 
-    // permettra d'implémenter les services que nous allons créer:
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .client(okHttpClient)
         .addConverterFactory(converterFactory)
         .build()
 
-    // Service to query API on users.
+    // Service for user-related queries to the API.
     val userWebService: UserWebService by lazy {
         retrofit.create(UserWebService::class.java)
     }
 
-    // Service to query API on tasks.
+    // Service for task-related queries to the API.
     val tasksWebService: TasksWebService by lazy {
         retrofit.create(TasksWebService::class.java)
-    }
-
-    lateinit var appContext: Context
-
-    fun setUpContext(context: Context) {
-        appContext = context
     }
 
 }
