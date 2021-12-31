7b6865ee53a77ad1bbb32d2750cd811892d55fdf
Gibril Gunder <gibril.gunder@telecom-sudparis.eu>
2021-12-31 11:19:44 +0100

    Cleaned FormActivity and its layout file

--- app/src/main/java/com/gib/filrouge/form/FormActivity.kt
+++ app/src/main/java/com/gib/filrouge/form/FormActivity.kt
@@ -2,51 +2,80 @@ package com.gib.filrouge.form
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import com.gib.filrouge.R
 import com.gib.filrouge.tasklist.Task
 import java.util.*
 
+// The activity launched when the user
+// needs to add or edit a task.
 class FormActivity : AppCompatActivity() {
+
+    // The Task instance received from the main activity
+    // (task list fragment) in case the user wants to edit
+    // a specific task.
+    // This attribute is null if the user just wants to
+    // add a new task.
+    private var task: Task? = null
+
+    // The different views in the activity's layout.
+    private lateinit var text: EditText
+    private lateinit var taskNameField: EditText
+    private lateinit var taskDescriptionField: EditText
+    private lateinit var okButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
 
-        // The different view elements in the form activity.
-        val taskNameInput = findViewById<EditText>(R.id.taskNameInput);
-        val taskDescriptionInput = findViewById<EditText>(R.id.taskDescriptionInput);
-        val validateButton = findViewById<Button>(R.id.validateTaskCreation);
+        // Retrieving the different views in the activity's
+        // layout.
+        text = findViewById(R.id.activity_form_text)
+        taskNameField = findViewById<EditText>(R.id.activity_form_task_name_field);
+        taskDescriptionField = findViewById<EditText>(R.id.activity_form_task_description_field);
+        okButton = findViewById<Button>(R.id.activity_form_ok_button);
 
-        // Retrieves the task embedded in the received intent if such a task exists.
-        // If the intent does not contain any task, we are creating a new task.
-        // Otherwise, we are editing an existing task.
-        var task: Task? = intent.getSerializableExtra("task") as? Task?;
+        okButton.setOnClickListener {
+            // We create a new Task instance with the
+            // data provided by the user in the form's fields,
+            // regardless of whether the user edits an existing task,
+            // or creates a new one.
+            val newTask = Task(task?.id ?: UUID.randomUUID().toString(), taskNameField.text.toString(), taskDescriptionField.text.toString())
 
-        // Retrieves the task title and description if the intent
-        // contains a task instance.
-        val taskTitle = task?.title ?: "New task";
-        val taskDescription = task?.description ?: "This is a new task!";
-
-        // Filling the input fields.
-        taskNameInput.setText(taskTitle);
-        taskDescriptionInput.setText(taskDescription);
-
-        validateButton.setOnClickListener {
-
-            // Retrieves user inputs in the text fields.
-            val taskTitle = taskNameInput.text.toString();
-            val taskDescription = taskDescriptionInput.text.toString();
-
-            // Creates a new Task instance.
-            val newTask = Task(id = task?.id ?: UUID.randomUUID().toString(), title = taskTitle, description = taskDescription);
-
-            // Filling the returned intent with the newly created Task instance.
-            intent.putExtra("task", newTask);
+            // The activity returns an intent to the main activity
+            // before it finishes. This intent contains the newly
+            // created task instance.
+            intent.putExtra("task", newTask)
             setResult(RESULT_OK, intent);
             finish();
-        };
+        }
     }
+
+    override fun onResume() {
+        super.onResume()
+
+        // When the activity is resumed, we retrieve the
+        // Task instance that is embedded in the intent sent by
+        // the main activity in the task list fragment.
+        task = intent.getSerializableExtra("task") as Task?
+
+        // We then adapt the layout of the activity based on
+        // whether the task received is null
+        // (user wants to add a new task) or not (user wants
+        // to edit an existing task).
+        if(task == null) {
+            text.setText("Hooray! Let's add a new task!")
+            taskNameField.hint = "Task name"
+            taskDescriptionField.hint = "Task description"
+            okButton.setText("Create")
+        }
+        else {
+            text.setText("Let's change a few things here...")
+            taskNameField.setText(task?.title)
+            taskDescriptionField.setText(task?.description)
+            okButton.setText("Save")
+        }
+    }
+
 }
